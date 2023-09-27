e71728a13ad25f7935b73a5b80900d0116ab34bf
Tayyab <tayyab.saeed@devsinc.com>
2023-09-27 20:02:46 +0500

    DONE: _Layout, Welcome (Half done)

--- app/index.js
+++ app/index.js
@@ -11,9 +11,35 @@ import {
 } from "../components";
 
 export default function Home() {
+
+  const router = useRouter(); 
   return (
-    <SafeAreaView>
-      <Text>Home</Text>
+    <SafeAreaView style = {{flex: 1 , backgroundColor: COLORS.lightWhite}}>
+      <Stack.Screen
+      options={{
+        headerStyle: {
+          backgroundColor: COLORS.lightWhite,
+        },
+        headerShadowVisible: false,
+        headerLeft: () => {
+          return <ScreenHeaderBtn iconUrl={icons.menu} dimensions="60%"/>
+        },
+
+        headerRight: () => {
+          return <ScreenHeaderBtn iconUrl={images.profile} dimensions="100%"/>
+        },
+        headerTitle: "",
+      }}
+    
+      />
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style = {{flex: 1, padding: SIZES.medium}}>
+          <Welcome />
+          <Popularjobs />
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,14 +1,39 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { View, Text, TouchableOpacity, Image } from "react-native";
 
-import styles from './welcome.style'
+import styles from "./welcome.style";
+import { TextInput } from "react-native";
+import { icons } from "../../../constants";
 
 const Welcome = () => {
   return (
     <View>
-      <Text>Welcome</Text>
-    </View>
-  )
-}
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Tayyab</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
 
-export default Welcome
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            value=""
+            onChange={() => {}}
+            placeholder="What are you looking for?"
+          />
+        </View>
+        <TouchableOpacity style={styles.searchBtn}>
+          <Image
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+
+      
+    </View>
+  );
+};
+
+export default Welcome;
