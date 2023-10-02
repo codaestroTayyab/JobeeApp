a34ed6096302cfeefe644595f9a67414b190f1c7
Tayyab <tayyab.saeed@devsinc.com>
2023-10-02 22:09:35 +0500

    ADDED: Categories, PopularJob

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,14 +1,53 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-
-import styles from './popularjobs.style'
+import React from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  ActivityIndicator,
+  TouchableOpacity,
+} from "react-native";
+import { COLORS, SIZES } from "../../../constants";
+import PopularJobCard from "../../../components/common/cards/popular/PopularJobCard";
+import styles from "./popularjobs.style";
+import { useRouter } from "expo-router";
 
 const Popularjobs = () => {
-  return (
-    <View>
-      <Text>Popularjobs</Text>
-    </View>
-  )
-}
+  const router = useRouter();
+  const isLoading = false;
+  const error = false;
 
-export default Popularjobs
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popularjobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>View All</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size="large" color={COLORS.primary} />
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : (
+          <FlatList
+          data={[1, 2, 3, 4, 5, 6, 7, 8]}
+          renderItem={({ item }) => {
+            return <PopularJobCard item={item} />;
+          }}
+          keyExtractor={(item) => item?.jobId}
+          contentContainerStyle={{columnGap: SIZES.medium}}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+
+        />
+        )}
+        
+
+      </View>
+    </View>
+  );
+};
+
+export default Popularjobs;
--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, TouchableOpacity, Image } from "react-native";
 
 import styles from "./welcome.style";
 import { TextInput } from "react-native";
-import { icons } from "../../../constants";
+import { SIZES, icons } from "../../../constants";
+import { FlatList } from "react-native-gesture-handler";
+import { useRouter } from "expo-router";
 
 const Welcome = () => {
+  const jobTypes = [
+    "Full-Time",
+    "Part-Time",
+    "Contractor",
+    "Internship",
+    "Temporary",
+  ];
+  const [activeJobType, setActiveJobType] = useState("Full-Time");
+
+  const router = useRouter();
   return (
     <View>
       <View style={styles.container}>
@@ -31,7 +43,28 @@ const Welcome = () => {
         </TouchableOpacity>
       </View>
 
-      
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={({ item }) => {
+            return (
+              <TouchableOpacity
+                style={styles.tab(activeJobType, item)}
+                onPress={() => {
+                  setActiveJobType(item);
+                  router.push(`/search/${item}`);
+                }}
+              >
+                <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+              </TouchableOpacity>
+            );
+          }}
+          keyExtractor={(item) => item}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{columnGap: SIZES.small}}
+        />
+      </View>
     </View>
   );
 };
